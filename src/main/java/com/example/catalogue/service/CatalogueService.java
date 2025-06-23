6aa1c4e79b1a40f30bc664434e1c68b2d6f2200a
Tomasz Laboj <tomasz.laboj@ocado.com>
2025-06-23 23:23:11 +0100

    add service methods

--- src/main/java/com/example/catalogue/service/CatalogueService.java
+++ src/main/java/com/example/catalogue/service/CatalogueService.java
@@ -1,6 +1,7 @@
 package com.example.catalogue.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,27 @@ public class CatalogueService {
         this.catalogueRepository = catalogueRepository;
     }
 
-    List<ProductRaw> getProducts(String name) {
+    ProductRaw getProductById(Long id) {
+        return catalogueRepository.findById(id).isPresent() ? catalogueRepository.findById(id).get().toRaw() : null;
+    };
+
+    List<ProductRaw> getProductByName(String name) {
+        List<ProductRaw> products = catalogueRepository.findProductsByCategory(name)
+                .stream()
+                .map(product -> product.toRaw())
+                .collect(Collectors.toList());
+        return products;
+    }
+    List<ProductRaw> getAllProducts() {
         List<ProductRaw> products = catalogueRepository.findAll()
+                .stream()
+                .map(product -> product.toRaw())
+                .collect(Collectors.toList());
+        return products;
+    };
+
+    List<ProductRaw> getProductsByCategory(String category) {
+        List<ProductRaw> products = catalogueRepository.findProductsByCategory(category)
                 .stream()
                 .map(product -> product.toRaw())
                 .collect(Collectors.toList());
