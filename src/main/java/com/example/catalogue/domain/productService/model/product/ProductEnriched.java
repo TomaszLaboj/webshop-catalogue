79cfac188c2bc3887b82dfcc177ea4cd8cc845f5
Tomasz Laboj <tomasz.laboj@ocado.com>
2025-12-08 23:12:12 +0000

    improve price per unit calculation

--- src/main/java/com/example/catalogue/domain/productService/model/product/ProductEnriched.java
+++ src/main/java/com/example/catalogue/domain/productService/model/product/ProductEnriched.java
@@ -53,16 +53,26 @@ public class ProductEnriched {
     PricePerUnit calculatePricePerUnit(int numberOfItemsInPack, float price, int measureCount, String unitOfMeasure) {
         float pricePerUnit = 0;
         int unitCount = 0;
+        String convertedUnitOfMeasure = "";
 
         if (unitOfMeasure.toLowerCase().equals("kg") || unitOfMeasure.toLowerCase().equals("l")) {
-            pricePerUnit = price / measureCount;
-            unitCount = 1;
-        }
-        else if (unitOfMeasure.toLowerCase().equals("g") || unitOfMeasure.toLowerCase().equals("ml")) {
-            pricePerUnit = price / measureCount * 100;
+            float pricePerGram = price / Math.multiplyExact(numberOfItemsInPack, Math.multiplyExact(measureCount, 1000));
+            pricePerUnit = pricePerGram * 100;
+            convertedUnitOfMeasure = unitOfMeasure == "kg" ? "g" : "ml";
             unitCount = 100;
         }
-        return new PricePerUnit(pricePerUnit, unitCount, unitOfMeasure);
+        else if (unitOfMeasure.toLowerCase().equals("g") || unitOfMeasure.toLowerCase().equals("ml")) {
+            float pricePerGram = price / Math.multiplyExact(numberOfItemsInPack, measureCount);
+            pricePerUnit = pricePerGram * 100;
+            convertedUnitOfMeasure = unitOfMeasure;
+            unitCount = 100;
+        }
+        else if (unitOfMeasure.toLowerCase().equals("item")) {
+            pricePerUnit = price / Math.multiplyExact(numberOfItemsInPack, measureCount);
+            unitCount = 1;
+        }
+        float roundedPrice = (float) (Math.round(pricePerUnit * Math.pow(10, 2)) / Math.pow(10, 2));
+        return new PricePerUnit(roundedPrice, unitCount, convertedUnitOfMeasure);
     }
 
 }
